fd80267af36ade8ae801ee1d4c32f48e2a481e21
FxYCarl <fxycarl@gmail.com>
2013-02-17 20:05:23 +0800

    Complete Config

--- Web/Config/Database.php
+++ Web/Config/Database.php
@@ -1,12 +1,15 @@
 <?php
 require_once dirname(__FILE__).'Config.php';
 //According to different databases,choose different database adapters.
-define("DATABASE_NOT_POOL",0);
-if (defined("DATABASE_MYSQL")){
-    define("DEFAULT_HOST",127.0.0.1);
+if (defined("DATABASE_MYSQL_DEFAULT")){
+    define("DEFAULT_MASTER_HOST",127.0.0.1);
+    if(defined("DATABASE_MULTI_HOST")){
+        define("DEFAULT_SLAVE_HOST",127.0.0.1);
+    }
+    define("DEFAULT_HOST",DEFAULT_MASTER_HOST);
     define("DEFAULT_PORT",3306);
     define("DEFAULT_USER",'root');
-    define("DEFAULT_PASSWD",'');
-    define("DEFAULT_RESULT_STYLE",'array');
+    define("DEFAULT_PASSWD",'root');
+    define("DEFAULT_RESULT_STYLE_ARRAY",1);
 }
 
--- Web/Config/Metadata.php
+++ Web/Config/Metadata.php
@@ -1,6 +1,5 @@
 <?php
-    require_once dirname(__FILE__).'Config.php';
+require_once dirname(__FILE__).'/Config.php';
     
-?>
 
 
