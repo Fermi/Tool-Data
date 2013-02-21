fc6abd851a5bfd73215f8c13991b89d57b8d23f3
FxYCarl <fxycarl@gmail.com>
2013-02-21 23:19:24 +0800

    DatabaseMysqli Finished

--- Web/Database/DatabaseMysqli.class.php
+++ Web/Database/DatabaseMysqli.class.php
@@ -1,19 +1,56 @@
 <?php
 
 class DatabaseMysqli {
-    public static function connectDB($dbhost,$dbport,$dbname){
-           
+    public static function connectDB($dbhost=DEFAULT_HOST,$dbport=DEFAULT_PORT,$dbname){
+        if(isset($dbname)){
+            return new mysqli($dbhost,DEFAULT_USER,DEFAULT_PASSWD,$dbname,$dbport);
+        } else {
+            return null;
+        }
     }
     public static function executeDBQuery($dbconn,$sql,$encoding){
-        
-    }
+        if(isset($encoding)){
+            //TODO:Encoding legal check.
+            mysqli_set_charset($dbconn,$encoding);
+        }
+        if(isset($sql)){
+            $result_set = mysqli_query($dbconn,$sql->getSqlString());
+            if(defined('DEFAULT_RESULT_STYLE_ARRAY')){
+                $result = mysqli_fetch_array($result_set);
+            } else {
+                $result = mysqli_fetch_object($result_set);
+            }
+            return $result;
+        } else{
+            return null;
+        }
+    } 
     public static function executeDBQueryArray($dbconn,$sqlarray,$encoding){
-        
+        $result = array();
+        if(isset($encoding)){
+            //TODO:Encoding legal check.
+            mysqli_set_charset($dbconn,$encoding);
+        }
+        if(isset($sqlarray)&&is_array($sqlarray)){
+            foreach($sqlarray as $sql){
+                $result_set = mysqli_query($dbconn,$sql->getSqlString());
+                if(defined('DEFAULT_RESULT_STYLE_ARRAY')){
+                    $tmp_result = mysqli_fetch_array($result_set);
+                } else {
+                    $tmp_result = mysqli_fetch_object($result_set);
+                }
+                $result[] = $tmp_result;
+            }
+        } else {
+            return null;
+        }
+
+        return $result;
     }
     public static function leaveDB($dbconn){
-        
+        $dbconn->close(); 
     }
     public static function getError($dbconn){
-        
+        return $dbconn->error_list;
     }
 }
